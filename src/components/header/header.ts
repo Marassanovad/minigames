16249feb3f89e25c432297aad57f952ef536ef9c
Marassanovad <maradasha@mail.ru>
2026-09-20 10:04:47 +0500

    feat: add burger menu modal

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -2,7 +2,11 @@ import { createAuthButton } from '../auth-button/auth-button';
 import { createBurgerMenuButton } from '../burger-menu-button/burger-menu-button';
 import './header.scss';
 import logoIcon from '../../assets/icons/logo.svg';
-import { navigationLinks } from '../../app/navigation-links.ts';
+import { navigationLinks } from '../../data/navigation-links.ts';
+import {
+  createMobileMenu,
+  openMenu,
+} from '../burger-menu-button/mobile-menu/mobile-menu.ts';
 
 interface HeaderOptions {
   userName?: string;
@@ -57,9 +61,19 @@ export function createHeader({
     navActions.append(actions);
   }
 
-  const burgerButton = createBurgerMenuButton(onMenuClick);
-  burgerButton.className = 'header__burger-menu';
+  const mobileMenu = createMobileMenu({
+    isAuthenticated,
+  });
+
+  const burgerButton = createBurgerMenuButton(() => {
+    openMenu(mobileMenu);
+    onMenuClick();
+  });
+  burgerButton.classList.add('header__burger-menu');
+
   navActions.append(burgerButton);
+
+  document.body.append(mobileMenu);
 
   header.append(logo, navActions);
 
